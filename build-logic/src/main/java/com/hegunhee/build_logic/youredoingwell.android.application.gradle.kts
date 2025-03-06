7712d879e40dd1885b1eb992ed97d0d8f6792d07
이건희 <leech9876@naver.com>
2025-03-06 17:43:08 +0900

    #2 - build : app모듈의 build.gradle에 정의되어있던 의존성들 custom plugin으로 이동

--- build-logic/src/main/java/com/hegunhee/build_logic/youredoingwell.android.application.gradle.kts
+++ build-logic/src/main/java/com/hegunhee/build_logic/youredoingwell.android.application.gradle.kts
@@ -1,6 +1,7 @@
 import com.hegunhee.build_logic.setting.configureComposeAndroid
 import com.hegunhee.build_logic.setting.configureHilt
 import com.hegunhee.build_logic.setting.configureKotlinAndroid
+import com.hegunhee.build_logic.setup.libs
 
 plugins {
     id("com.android.application")
@@ -9,3 +10,11 @@ plugins {
 configureKotlinAndroid()
 configureComposeAndroid()
 configureHilt()
+
+dependencies {
+
+    add("implementation", libs.findLibrary("androidx-core-ktx").get())
+    add("implementation", libs.findLibrary("androidx-lifecycle-runtime-ktx").get())
+    add("implementation", libs.findLibrary("androidx-activity-compose").get())
+
+}
