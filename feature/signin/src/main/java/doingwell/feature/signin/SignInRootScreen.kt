a76d61acb1fda038d8ed209ee9eb7776b69fca5b
이건희 <leech9876@naver.com>
2025-03-26 22:32:22 +0900

    #12 - feat : SignInRootScreen에서 SignInScreen을 호출하도록 수정

--- feature/signin/src/main/java/doingwell/feature/signin/SignInRootScreen.kt
+++ feature/signin/src/main/java/doingwell/feature/signin/SignInRootScreen.kt
@@ -3,12 +3,16 @@ package doingwell.feature.signin
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SignInRootScreen(
     paddingValues: PaddingValues,
 ) {
-
+    SignInScreen(
+        paddingValues = paddingValues,
+    )
 }
 
 @Composable
@@ -16,4 +20,12 @@ fun SignInScreen(
     paddingValues: PaddingValues
 ) {
     Text("로그인 화면 입니다.")
+}
+
+@Preview
+@Composable
+fun SignInScreenPreview() {
+    SignInScreen(
+        paddingValues = PaddingValues(10.dp)
+    )
 }
