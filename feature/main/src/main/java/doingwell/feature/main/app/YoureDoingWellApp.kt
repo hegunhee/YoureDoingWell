d180c47dec3065d1405a5071dfcbc50f1c42c657
이건희 <leech9876@naver.com>
2025-03-26 16:57:41 +0900

    #8 - feat : 최상위 컴포저블인 App 작성

--- feature/main/src/main/java/doingwell/feature/main/app/YoureDoingWellApp.kt
+++ feature/main/src/main/java/doingwell/feature/main/app/YoureDoingWellApp.kt
@@ -1,11 +1,35 @@
 package doingwell.feature.main.app
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import doingwell.feature.main.screen.MainScreenRoot
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import doingwell.feature.main.screen.navigation.MAIN_ROUTE
+import doingwell.feature.main.screen.navigation.mainNavGraph
 
 @Composable
 fun YoureDoingWellApp(
-
+    youreDoingWellAppState: YoureDoingWellAppState = rememberYoureDoingWellAppState()
 ) {
-    MainScreenRoot()
+    Scaffold { paddingValues ->
+        NavHost(
+            navController = youreDoingWellAppState.navController,
+            startDestination = MAIN_ROUTE
+        ) {
+            mainNavGraph(paddingValues)
+        }
+    }
+}
+
+@Composable
+fun rememberYoureDoingWellAppState(
+    navController: NavHostController = rememberNavController(),
+): YoureDoingWellAppState {
+    return remember(navController) {
+        YoureDoingWellAppState(
+            navController
+        )
+    }
 }
