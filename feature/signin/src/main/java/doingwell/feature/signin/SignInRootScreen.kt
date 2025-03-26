4586fbd3fd76157f4481535f8441f845c0d1b6d7
이건희 <leech9876@naver.com>
2025-03-26 22:22:52 +0900

    #12 - feat : SignInRootScreen 뼈대 작성

---
+++ feature/signin/src/main/java/doingwell/feature/signin/SignInRootScreen.kt
@@ -0,0 +1,19 @@
+package doingwell.feature.signin
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SignInRootScreen(
+    paddingValues: PaddingValues,
+) {
+
+}
+
+@Composable
+fun SignInScreen(
+    paddingValues: PaddingValues
+) {
+    Text("로그인 화면 입니다.")
+}
