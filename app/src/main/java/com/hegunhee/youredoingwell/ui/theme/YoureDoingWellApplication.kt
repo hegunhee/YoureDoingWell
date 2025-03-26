3d0a0c791119374e3dbc44e2c732d7e293949baf
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-03-26 20:19:21 +0900

    Hilt App, HiltViewModel 설정 (#11)
    * #10 - feat : HiltApp 설정
    
    * #10 - feat : MainActivity에 AndroidEntryPoint 등록
    
    * #10 - build : hiltViewModel을 위한 의존성 추가
    
    * #10 - build : hiltViewModel 의존성 feature 모듈들에 적용
    
    * #10 - feat : hiltViewModel이 잘 적용됬는지 테스트

---
+++ app/src/main/java/com/hegunhee/youredoingwell/ui/theme/YoureDoingWellApplication.kt
@@ -0,0 +1,8 @@
+package com.hegunhee.youredoingwell.ui.theme
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class YoureDoingWellApplication : Application() {
+}
---
+++ feature/main/src/main/java/doingwell/feature/main/screen/MainViewModel.kt
@@ -0,0 +1,12 @@
+package doingwell.feature.main.screen
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+
+) : ViewModel(){
+
+}
