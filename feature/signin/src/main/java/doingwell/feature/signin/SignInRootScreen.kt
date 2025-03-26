3f5a8a9bf09f785095564d6ad634c093f1215e27
이건희 <leech9876@naver.com>
2025-03-26 23:41:50 +0900

    #12 - feat : SignIn Screen UI 로그인까지 작성

--- feature/signin/src/main/java/doingwell/feature/signin/SignInRootScreen.kt
+++ feature/signin/src/main/java/doingwell/feature/signin/SignInRootScreen.kt
@@ -1,10 +1,29 @@
 package doingwell.feature.signin
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hegunhee.youredoingwell.ui.theme.MainGreen
+import com.hegunhee.youredoingwell.ui.theme.Typography
 
 @Composable
 fun SignInRootScreen(
@@ -17,9 +36,73 @@ fun SignInRootScreen(
 
 @Composable
 fun SignInScreen(
-    paddingValues: PaddingValues
+    paddingValues: PaddingValues,
+    modifier: Modifier = Modifier,
 ) {
-    Text("로그인 화면 입니다.")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Spacer(modifier = modifier.padding(top = 100.dp))
+        Text(
+            titleString(),
+            style = Typography.titleLarge,
+        )
+
+        Spacer(modifier = modifier.padding(top = 15.dp))
+        val itemModifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+            .padding(top = 10.dp)
+
+        OutlinedTextField(
+            "",
+            onValueChange = {},
+            placeholder = { Text(stringResource(R.string.enter_email)) },
+            modifier = itemModifier,
+        )
+
+        OutlinedTextField(
+            "",
+            onValueChange = {},
+            placeholder = { Text(stringResource(R.string.enter_password)) },
+            modifier = itemModifier,
+        )
+
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = MainGreen),
+            modifier = itemModifier,
+        ) {
+            Text(
+                text = "로그인",
+                color = Color.White,
+            )
+        }
+
+        Row(
+            modifier = itemModifier,
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Text(stringResource(R.string.find_password))
+            Spacer(modifier = modifier.padding(horizontal = 10.dp))
+            Text(stringResource(R.string.sign_up))
+        }
+    }
+}
+
+private fun titleString(): AnnotatedString {
+    return buildAnnotatedString {
+        withStyle(style = SpanStyle(color = MainGreen)) {
+            append("당")
+        }
+        append("신은")
+        append("\n")
+        withStyle(style = SpanStyle(color = MainGreen)) {
+            append("잘")
+        }
+        append("하고 있다")
+    }
 }
 
 @Preview
