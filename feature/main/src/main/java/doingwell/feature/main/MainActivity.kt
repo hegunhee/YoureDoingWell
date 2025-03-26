38dbed5bea202a77cb1520238b0c266a546aef49
이건희 <leech9876@naver.com>
2025-03-26 17:09:14 +0900

    #10 - feat : MainActivity에 AndroidEntryPoint 등록

--- feature/main/src/main/java/doingwell/feature/main/MainActivity.kt
+++ feature/main/src/main/java/doingwell/feature/main/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.hegunhee.youredoingwell.ui.theme.YoureDoingWellTheme
+import dagger.hilt.android.AndroidEntryPoint
 import doingwell.feature.main.app.YoureDoingWellApp
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
