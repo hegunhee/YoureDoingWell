01be68e1ba58dd28f45b8e17a5a7e294164c20ab
이건희 <leech9876@naver.com>
2025-03-27 22:46:49 +0900

    #14 - feat : TitleString을 core:ui에 정의
    그러므로 MainScreen과 SignInScreen에서 정의한것은 지움

--- feature/main/src/main/java/doingwell/feature/main/screen/MainRootScreen.kt
+++ feature/main/src/main/java/doingwell/feature/main/screen/MainRootScreen.kt
@@ -2,21 +2,15 @@ package doingwell.feature.main.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.hegunhee.youredoingwell.ui.theme.MainGreen
 import com.hegunhee.youredoingwell.ui.theme.Typography
 import doingwell.core.ui.text.TitleText
 
@@ -42,25 +36,10 @@ fun MainScreen(
         popSignInScreen()
     }
     Column {
-        Text(
-            text = titleString(),
+        TitleText(
             modifier = modifier.padding(start = 40.dp, top = 100.dp),
-            style = Typography.titleLarge
+            textStyle = Typography.titleLarge
         )
-    }
-}
-
-private fun titleString() : AnnotatedString {
-    return buildAnnotatedString {
-        withStyle(style = SpanStyle(color = MainGreen)) {
-            append("당")
-        }
-        append("신은")
-        append("\n")
-        withStyle(style = SpanStyle(color = MainGreen)) {
-            append("잘")
-        }
-        append("하고 있다")
     }
 }
 
--- feature/signin/src/main/java/doingwell/feature/signin/SignInRootScreen.kt
+++ feature/signin/src/main/java/doingwell/feature/signin/SignInRootScreen.kt
@@ -16,16 +16,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hegunhee.youredoingwell.ui.theme.MainGreen
 import com.hegunhee.youredoingwell.ui.theme.Typography
+import doingwell.core.ui.text.TitleText
 
 @Composable
 fun SignInRootScreen(
@@ -46,9 +42,8 @@ fun SignInScreen(
         modifier = modifier.fillMaxWidth()
     ) {
         Spacer(modifier = modifier.padding(top = 100.dp))
-        Text(
-            titleString(),
-            style = Typography.titleLarge,
+        TitleText(
+            textStyle = Typography.titleLarge,
         )
 
         Spacer(modifier = modifier.padding(top = 15.dp))
@@ -91,20 +86,6 @@ fun SignInScreen(
             Spacer(modifier = modifier.padding(horizontal = 10.dp))
             Text(stringResource(R.string.sign_up))
         }
-    }
-}
-
-private fun titleString(): AnnotatedString {
-    return buildAnnotatedString {
-        withStyle(style = SpanStyle(color = MainGreen)) {
-            append("당")
-        }
-        append("신은")
-        append("\n")
-        withStyle(style = SpanStyle(color = MainGreen)) {
-            append("잘")
-        }
-        append("하고 있다")
     }
 }
 
