3a911f18d730531c9f818da0d63cd85fa0c90670
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-03-28 15:46:46 +0900

    회원가입 로직 작성 (#19)
    * #18 - feat : Auth를 담당하는 State 작성
    
    * #18 - feat : App에서 AuthState 사용함
    
    * #18 - feat : SignUp 함수의 변화에 따라 onClickSignUpButton 함수 명세도 변경
    
    * #18 - feat : 로그인 화면의 회원가입 버튼에 인자로 받은 리스너 등록
    
    * #18 - feat : App에서 회원가입 함수들 등록
    
    * #18 - feat : 이메일이 중복일 경우 토스트메시지 보여줌
    
    * #18 - feat : 메인 화면에서 무조건 로그인 화면으로 이동하도록 주석 처리
    
    추후 메인화면이 생기면 변경
    
    * #18 - feat : 회원가입 성공시 추후에 db에 저장하도록 주석 추가

--- feature/main/src/main/java/doingwell/feature/main/app/YoureDoingWellApp.kt
+++ feature/main/src/main/java/doingwell/feature/main/app/YoureDoingWellApp.kt
@@ -1,18 +1,23 @@
 package doingwell.feature.main.app
 
+import android.content.Context
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import doingwell.feature.main.screen.navigation.MAIN_ROUTE
 import doingwell.feature.main.screen.navigation.mainNavGraph
 import doingwell.feature.signin.navigation.signInNavGraph
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun YoureDoingWellApp(
-    youreDoingWellAppState: YoureDoingWellAppState = rememberYoureDoingWellAppState()
+    youreDoingWellAppState: YoureDoingWellAppState = rememberYoureDoingWellAppState(),
+    youreDoingWellAuthState: YoureDoingWellAuthState = rememberYoureDoingWellAuthState(),
 ) {
     Scaffold { paddingValues ->
         NavHost(
@@ -26,6 +31,9 @@ fun YoureDoingWellApp(
 
             signInNavGraph(
                 paddingValues = paddingValues,
+                onClickSignUpScreenButton = youreDoingWellAppState::navigateToSignUp,
+                popUpBack = youreDoingWellAppState::popBackStack,
+                onClickSignUpButton = youreDoingWellAuthState::signUpWithEmail,
             )
         }
     }
@@ -39,5 +47,15 @@ fun rememberYoureDoingWellAppState(
         YoureDoingWellAppState(
             navController
         )
+    }
+}
+
+@Composable
+fun rememberYoureDoingWellAuthState(
+    context: Context = LocalContext.current,
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+): YoureDoingWellAuthState {
+    return remember(context, coroutineScope) {
+        YoureDoingWellAuthState(context, coroutineScope)
     }
 }
---
+++ feature/main/src/main/java/doingwell/feature/main/app/YoureDoingWellAuthState.kt
@@ -0,0 +1,53 @@
+package doingwell.feature.main.app
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import doingwell.feature.main.R
+import doingwell.feature.signin.isValidEmail
+import kotlinx.coroutines.CoroutineScope
+
+class YoureDoingWellAuthState(
+    val context: Context,
+    val coroutineScope: CoroutineScope
+) {
+    val auth = Firebase.auth
+
+    fun signUpWithEmail(
+        email: String,
+        password: String,
+        reCheckPassword: String,
+        successCallback: () -> Unit
+    ) {
+        if (email.isBlank() || password.isBlank()) {
+            context.toastMessage(R.string.email_or_password_empty)
+            return
+        }
+        if (!isValidEmail(email)) {
+            context.toastMessage(R.string.email_format_incorrect)
+            return
+        }
+        if (password != reCheckPassword) {
+            context.toastMessage(R.string.password_recheck_password_is_not_same)
+            return
+        }
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                context.toastMessage(R.string.signup_success)
+                // db에 uid 등록
+                successCallback()
+            }.addOnFailureListener {
+                if (it is FirebaseAuthUserCollisionException) {
+                    context.toastMessage(R.string.email_exist)
+                }
+            }
+    }
+
+    private fun Context.toastMessage(@StringRes stringRes: Int) {
+        Toast.makeText(this, getText(stringRes), Toast.LENGTH_SHORT).show()
+    }
+}
