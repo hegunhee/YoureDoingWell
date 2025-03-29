1a2abe1581dbe06235d58ab643ef0b4474aaeb36
이건희 <leech9876@naver.com>
2025-03-29 11:11:03 +0900

    #22 - feat : SignInState, SignInState StateFlow 정의
    UserData는 실질적으로 유저들에게 보내줄 정보고
    SignInState는 App에서만 사용할 정보다

--- feature/main/src/main/java/doingwell/feature/main/app/YoureDoingWellAuthState.kt
+++ feature/main/src/main/java/doingwell/feature/main/app/YoureDoingWellAuthState.kt
@@ -4,17 +4,42 @@ import android.content.Context
 import android.widget.Toast
 import androidx.annotation.StringRes
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.hegunhee.model.user.UserData
 import doingwell.feature.main.R
 import doingwell.feature.signin.isValidEmail
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class YoureDoingWellAuthState(
     val context: Context,
     val coroutineScope: CoroutineScope
 ) {
     val auth = Firebase.auth
+
+    private val _signInState: MutableStateFlow<SignInState> = MutableStateFlow(
+        auth.currentUser?.let { firebaseUser ->
+            SignInState(isSignIn = true, userData = firebaseUser.toUserData(), errorMessage = null)
+        } ?: run {
+            SignInState.DEFAULT
+        }
+    )
+    val signInState : StateFlow<SignInState> = _signInState.asStateFlow()
+
+    val userData : StateFlow<UserData?> = signInState.map {
+        it.userData
+    }.stateIn(
+        scope = coroutineScope,
+        initialValue = SignInState.DEFAULT.userData,
+        started = SharingStarted.WhileSubscribed(5_000)
+    )
 
     fun signInWithEmailAndPassword(email: String, password: String, successCallback: () -> Unit) {
         if (email.isBlank() || password.isBlank()) {
@@ -68,4 +93,22 @@ class YoureDoingWellAuthState(
     private fun Context.toastMessage(@StringRes stringRes: Int) {
         Toast.makeText(this, getText(stringRes), Toast.LENGTH_SHORT).show()
     }
+}
+
+data class SignInState(
+    val isSignIn: Boolean,
+    val userData: UserData?,
+    val errorMessage: String?,
+) {
+    companion object {
+        val DEFAULT = SignInState(
+            isSignIn = false,
+            userData = null,
+            errorMessage = null,
+        )
+    }
+}
+
+fun FirebaseUser.toUserData(): UserData {
+    return UserData(uid, email, photoUrl.toString())
 }
