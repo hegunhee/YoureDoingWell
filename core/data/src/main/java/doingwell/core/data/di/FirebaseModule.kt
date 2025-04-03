f2dcab4f46cfe24791673ad839cc0fc551c9a27e
이건희 <leech9876@naver.com>
2025-04-03 11:40:14 +0900

    #32 - feat : FirebaseModule에서 databaseReference를 주입

---
+++ core/data/src/main/java/doingwell/core/data/di/FirebaseModule.kt
@@ -0,0 +1,21 @@
+package doingwell.core.data.di
+
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object FirebaseModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseReference() : DatabaseReference {
+        return Firebase.database.reference
+    }
+}
