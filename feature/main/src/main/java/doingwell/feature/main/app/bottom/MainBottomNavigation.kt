03c132d7add96583462f6baf7eb2680670ff61a2
이건희 <leech9876@naver.com>
2025-04-09 15:14:22 +0900

    #40 - feat : BottomNavigation UI 작성

---
+++ feature/main/src/main/java/doingwell/feature/main/app/bottom/MainBottomNavigation.kt
@@ -0,0 +1,67 @@
+package doingwell.feature.main.app.bottom
+
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavBackStackEntry
+import doingwell.feature.main.R
+
+@Composable
+internal fun MainBottomNavigation(
+    modifier: Modifier = Modifier,
+    backStackEntry: State<NavBackStackEntry?>,
+    onItemClick: (String) -> Unit,
+) {
+    val currentRoute = backStackEntry.value?.destination?.route ?: ""
+    NavigationBar(
+        modifier = modifier,
+    ) {
+        BottomNavigationItem.items.forEach { item ->
+            if (item.title == R.string.add) {
+                FloatingActionButton({ onItemClick(item.screenRoute) }) {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = stringResource(item.title)
+                    )
+                }
+            } else {
+                NavigationBarItem(
+                    selected = currentRoute == item.screenRoute,
+                    label = {
+                        Text(
+                            text = stringResource(id = item.title),
+                            fontSize = 12.sp
+                        )
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = stringResource(item.title)
+                        )
+                    },
+                    onClick = {
+                        onItemClick(item.screenRoute)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MainBottomNavigationPreview() {
+    val backStackEntry = remember { mutableStateOf(null) }
+    MainBottomNavigation(backStackEntry = backStackEntry, onItemClick = {})
+}
