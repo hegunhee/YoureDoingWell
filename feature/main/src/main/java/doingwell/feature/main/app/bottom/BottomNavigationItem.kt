bcda61ee4dfc7d4f79316579089390a301d534d3
이건희 <leech9876@naver.com>
2025-04-09 14:48:48 +0900

    #40 - feat : BottomNavigationItem 작성
    현재 Daily, AddRecord, Setting이 존재함

--- feature/main/src/main/java/doingwell/feature/main/app/bottom/BottomNavigationItem.kt
+++ feature/main/src/main/java/doingwell/feature/main/app/bottom/BottomNavigationItem.kt
@@ -1,5 +1,26 @@
 package doingwell.feature.main.app.bottom
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.hegunhee.daily.navigation.DAILY_ROUTE
+import doingwell.feature.main.R
+
+sealed class BottomNavigationItem(
+    val title: Int,
+    val icon: ImageVector,
+    val screenRoute: String,
+) {
+    data object Daily : BottomNavigationItem(R.string.daily, Icons.Default.Home, DAILY_ROUTE)
+    data object AddRecord : BottomNavigationItem(R.string.add, Icons.Default.Add, ADD_RECORD_ROUTE)
+    data object Setting : BottomNavigationItem(R.string.setting, Icons.Default.Settings, SETTING_ROUTE)
+
+    companion object {
+        val items = listOf(Daily, AddRecord, Setting)
+    }
+}
 
 const val ADD_RECORD_ROUTE = "ADD_RECORD"
 const val SETTING_ROUTE = "SETTING"
