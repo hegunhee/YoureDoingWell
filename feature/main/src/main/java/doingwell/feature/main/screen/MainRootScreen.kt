ae82c8ef86f490fe58e4d7210cac919b998d53d5
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-04-15 15:37:52 +0900

    Main 화면 테스트코드 작성 (#44)
    * #43 - style : MainScreen 함수의 접근제어자 internal로 변경
    
    * #43 - test : MainScreen 테스트코드 작성
    
    앞으로 MainScreen에 코드가 추가될수록 해당 클래스에서 테스트를 작성할 예정이다
    
    * #43 - refactor : 사용하지않는 MainViewModel을 삭제했다
    
    예전에 MainViewModel을 작성했을때는
    hiltViewModel이 정상작동하는지 테스트를 위해 작성했지만
    현재는 아무런 필요가 없으므로 삭제했다.

--- feature/main/src/main/java/doingwell/feature/main/screen/MainRootScreen.kt
+++ feature/main/src/main/java/doingwell/feature/main/screen/MainRootScreen.kt
@@ -7,14 +7,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.hegunhee.model.user.UserData
 import com.hegunhee.youredoingwell.ui.theme.Typography
 import doingwell.core.ui.text.TitleText
 
 @Composable
 fun MainScreenRoot(
-    viewModel : MainViewModel = hiltViewModel(),
     userData: UserData?,
     popSignInScreen: () -> Unit,
     popDailyScreen: () -> Unit,
@@ -32,7 +30,7 @@ fun MainScreenRoot(
 }
 
 @Composable
-fun MainScreen(
+internal fun MainScreen(
     userData : UserData?,
     modifier : Modifier = Modifier,
 ) {
