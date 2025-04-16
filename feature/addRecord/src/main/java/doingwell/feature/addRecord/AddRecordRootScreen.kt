9e46fc68ea06d9946eaabe2a2e2be3335be302cb
이건희 <leech9876@naver.com>
2025-04-16 16:05:21 +0900

    #42 - feat : 사진들 새기록 화면에 등록
    사진은 최대 4개까지 등록 가능하도록 설정했다

--- feature/addRecord/src/main/java/doingwell/feature/addRecord/AddRecordRootScreen.kt
+++ feature/addRecord/src/main/java/doingwell/feature/addRecord/AddRecordRootScreen.kt
@@ -2,6 +2,8 @@ package doingwell.feature.addRecord
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hegunhee.model.user.UserData
+import doingwell.core.ui.text.item.SmallPhoto
 
 @Composable
 fun AddRecordRootScreen(
@@ -41,11 +44,27 @@ internal fun AddRecordScreen(
             fontSize = 20.sp
         )
         Text(
+            stringResource(R.string.photo),
+            fontSize = 15.sp,
+        )
+        Row(
+            modifier = modifier,
+        ) {
+            (0..3).forEach { _ ->
+                SmallPhoto(
+                    url = null,
+                    onClickEmptyPhoto = {},
+                    onClickDeletePhoto = {},
+                )
+                Spacer(modifier = modifier.padding(horizontal = 5.dp))
+            }
+
+        }
+        Text(
             userData.toString(),
             modifier = modifier.padding(paddingValues)
         )
     }
-
 }
 
 @Preview
