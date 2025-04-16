848a1755473fbc8577033eb004bb6f4e3b006bab
이건희 <leech9876@naver.com>
2025-04-16 15:26:21 +0900

    #42 - feat : 기록 추가 화면에 새 기록 텍스트 추가

--- feature/addRecord/src/main/java/doingwell/feature/addRecord/AddRecordRootScreen.kt
+++ feature/addRecord/src/main/java/doingwell/feature/addRecord/AddRecordRootScreen.kt
@@ -1,11 +1,15 @@
 package doingwell.feature.addRecord
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hegunhee.model.user.UserData
 
 @Composable
@@ -27,10 +31,21 @@ internal fun AddRecordScreen(
     userData: UserData,
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        userData.toString(),
-        modifier = modifier.padding(paddingValues)
-    )
+    Column(
+        modifier = modifier
+            .padding(paddingValues)
+            .padding(10.dp)
+    ) {
+        Text(
+            stringResource(R.string.new_record),
+            fontSize = 20.sp
+        )
+        Text(
+            userData.toString(),
+            modifier = modifier.padding(paddingValues)
+        )
+    }
+
 }
 
 @Preview
