10a1c70a98033d321b6de7ab525b61a3f6aeec5f
이건희 <leech9876@naver.com>
2025-04-25 17:17:58 +0900

    #54 - feat : 일일 기록 데이터 저장, 조회, 삭제 로직 작성, 테스트코드도 작성

---
+++ core/data/src/androidTest/java/doingwell/core/data/remote/DailyRecordRemoteDataSourceTest.kt
@@ -0,0 +1,120 @@
+package doingwell.core.data.remote
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.database.database
+import com.hegunhee.model.user.DateTimeInfo
+import doingwell.core.data.datasource.remote.DailyRecordRemoteDataSource
+import doingwell.core.data.datasource.remote.DefaultDailyRecordRemoteDataSource
+import doingwell.core.data.datasource.remote.model.DateTimeResponse
+import doingwell.core.data.datasource.remote.model.record.DailyRecordResponse
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.LocalDateTime
+
+@RunWith(AndroidJUnit4::class)
+class DailyRecordRemoteDataSourceTest {
+
+    private lateinit var sut: DailyRecordRemoteDataSource
+
+    private val now: LocalDateTime = LocalDateTime.now()
+
+    @Before
+    fun initContext() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        if (FirebaseApp.getApps(context).isEmpty()) {
+            val options = FirebaseOptions.Builder()
+                .setApplicationId(FirebaseTestParams.APPLICATION_ID)
+                .setApiKey(FirebaseTestParams.API_KEY)
+                .setDatabaseUrl(FirebaseTestParams.DATABASE_URL)
+                .build()
+            FirebaseApp.initializeApp(context, options)
+        }
+        sut = DefaultDailyRecordRemoteDataSource(Firebase.database.getReference("TEST"))
+    }
+
+    @Test
+    fun givenDailyRecordResponse_whenInserting_thenWorksFine() {
+        runBlocking {
+            // Given
+            val dailyRecordResponse = createDailyRecordResponse("userId", "title")
+
+            // When
+            val key = sut.insertDailyRecord(dailyRecordResponse)
+
+            // Then
+            assertEquals(key, dailyRecordResponse.key)
+            sut.deleteDailyRecord(
+                dailyRecordResponse.userId,
+                dailyRecordResponse.title,
+                dailyRecordResponse.startedAt.dateStamp,
+                dailyRecordResponse.startedAt.timeStamp
+            )
+        }
+    }
+
+    @Test
+    fun givenDailyRecordResponse_whenGetByUserId_thenWorksFine() {
+        runBlocking {
+            // Given
+            val userId = "userId123"
+
+            val dailyRecord1 = createDailyRecordResponse(userId, "title1")
+            val dailyRecord2 = createDailyRecordResponse(userId, "title2")
+
+            val beforeRecords = listOf(dailyRecord1, dailyRecord2)
+
+            sut.insertDailyRecord(dailyRecord1)
+            sut.insertDailyRecord(dailyRecord2)
+
+            // When
+            val records = sut.findDailyRecords(userId).sortedBy { it.title }
+
+            // Then
+            assertEquals(records.size, beforeRecords.size)
+            assertEquals(records, beforeRecords)
+
+            records.forEach { record ->
+                sut.deleteDailyRecord(
+                    record.userId,
+                    record.title,
+                    record.startedAt.dateStamp,
+                    record.startedAt.timeStamp
+                )
+            }
+        }
+    }
+
+    private fun createDailyRecordResponse(
+        userId: String,
+        title: String
+    ): DailyRecordResponse {
+        return DailyRecordResponse(
+            userId = userId,
+            title = title,
+            description = null,
+            photos = null,
+            startedAt = createDateTimeResponse(),
+            endedAt = createDateTimeResponse(),
+        )
+    }
+
+    private fun createDateTimeResponse(): DateTimeResponse {
+        return DateTimeResponse.of(
+            DateTimeInfo(
+                now.year,
+                now.monthValue,
+                now.dayOfMonth,
+                now.hour,
+                now.minute
+            )
+        )
+    }
+}
---
+++ core/data/src/main/java/doingwell/core/data/datasource/remote/DailyRecordRemoteDataSource.kt
@@ -0,0 +1,17 @@
+package doingwell.core.data.datasource.remote
+
+import doingwell.core.data.datasource.remote.model.record.DailyRecordResponse
+
+interface DailyRecordRemoteDataSource {
+
+    suspend fun insertDailyRecord(dailyRecordResponse: DailyRecordResponse): String
+
+    suspend fun findDailyRecords(userId: String): List<DailyRecordResponse>
+
+    suspend fun deleteDailyRecord(
+        userId: String,
+        title: String,
+        dateStamp: String,
+        timeStamp: String
+    ): String
+}
