fbdf1cb880757e778efcc9a86d01290c1d7b4890
이건희 <leech9876@naver.com>
2025-05-13 09:57:02 +0900

    #68 - feat : SmallPhoto에서 Url이 비어있는 경우를 삭제함
    SmallPhoto는 실제 값이 존재하는 경우에만 나타나며
    사진을 추가하는것은 별도의 UI로 작성할예정

--- core/ui/src/main/java/doingwell/core/ui/text/item/SmailPhoto.kt
+++ core/ui/src/main/java/doingwell/core/ui/text/item/SmailPhoto.kt
@@ -1,17 +1,12 @@
 package doingwell.core.ui.text.item
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.sharp.Add
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -19,61 +14,38 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import doingwell.core.ui.R
 
 @Composable
 fun SmallPhoto(
-    url: String?,
-    onClickEmptyPhoto: () -> Unit,
+    url: String,
+    onClickPhoto: (String) -> Unit,
     onClickDeletePhoto: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Box(
         modifier = modifier
-            .size(80.dp)
+            .size(dimensionResource(id = R.dimen.smallPhotoSize))
             .clip(RoundedCornerShape(5))
             .background(Color.Gray)
             .clickable {
-                if (url == null) {
-                    onClickEmptyPhoto()
-                }
+                onClickPhoto(url)
             }
     ) {
-        if(url == null) {
+        IconButton(
+            onClick = { onClickDeletePhoto(url) },
+            modifier = modifier.align(Alignment.TopEnd)
+        ) {
             Icon(
-                imageVector = Icons.Sharp.Add,
-                contentDescription = stringResource(R.string.add_photo),
-                tint = Color.White,
-                modifier = modifier.align(Alignment.Center)
+                imageVector = Icons.Filled.Close,
+                contentDescription = stringResource(R.string.delete_photo),
+                tint = Color.Red
             )
-        } else {
-            IconButton(
-                onClick = { onClickDeletePhoto(url)},
-                modifier = modifier.align(Alignment.TopEnd)
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.Close,
-                    contentDescription = stringResource(R.string.delete_photo),
-                    tint = Color.Red
-                )
-            }
         }
-
     }
-}
-
-@Preview
-@Composable
-private fun EmptySmallPhoto() {
-    SmallPhoto(
-        url = null,
-        onClickEmptyPhoto = {},
-        onClickDeletePhoto = {},
-        modifier = Modifier,
-    )
 }
 
 @Preview
@@ -82,7 +54,7 @@ private fun FillSmallPhoto() {
     val url = ""
     SmallPhoto(
         url = url,
-        onClickEmptyPhoto = {},
+        onClickPhoto = {},
         onClickDeletePhoto = {},
         modifier = Modifier,
     )
