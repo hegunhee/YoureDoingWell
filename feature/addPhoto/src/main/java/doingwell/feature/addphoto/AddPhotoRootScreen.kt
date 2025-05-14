501a51abec07e22fcc7e0471b32969b7c1332189
이건희 <leech9876@naver.com>
2025-05-14 23:30:42 +0900

    #70 - feat : 사진추가 화면에 백스택 버튼 추가

--- feature/addPhoto/src/main/java/doingwell/feature/addphoto/AddPhotoRootScreen.kt
+++ feature/addPhoto/src/main/java/doingwell/feature/addphoto/AddPhotoRootScreen.kt
@@ -2,8 +2,17 @@ package doingwell.feature.addphoto
 
 import android.net.Uri
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -23,6 +32,7 @@ fun AddPhotoRootScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun AddPhotoScreen(
     paddingValues: PaddingValues,
@@ -32,7 +42,26 @@ internal fun AddPhotoScreen(
     onClickAddPhotos: (ArrayList<Uri>) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-
+    Scaffold(
+        topBar = {
+          TopAppBar(
+              navigationIcon = {
+                  IconButton(
+                      onClick = onClickBackStack
+                  ) {
+                      Icon(
+                          Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                          contentDescription = stringResource(
+                              R.string.back_button
+                          )
+                      )
+                  }
+              },
+              title = { Text("전체 사진") },
+          )
+        },
+    ) { innerPadding ->
+    }
 }
 
 @Preview
