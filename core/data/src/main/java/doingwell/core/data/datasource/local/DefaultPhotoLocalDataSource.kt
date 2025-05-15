495371fd8d75aa656b38fa0510e3f010ab09a3a2
이건희 <leech9876@naver.com>
2025-05-16 00:40:38 +0900

    #74 - feat : 앨범 이름과 카운트수를 가져오는 코드 작성

---
+++ core/data/src/main/java/doingwell/core/data/datasource/local/DefaultPhotoLocalDataSource.kt
@@ -0,0 +1,54 @@
+package doingwell.core.data.datasource.local
+
+import android.content.Context
+import android.database.Cursor
+import android.provider.MediaStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import doingwell.core.data.datasource.local.model.AlbumSummaryResponse
+import doingwell.core.data.datasource.local.model.AlbumWithPhotosResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class DefaultPhotoLocalDataSource @Inject constructor(
+    @ApplicationContext private val context: Context,
+) : PhotoLocalDataSource {
+
+    override suspend fun getAlbumSummaryResponses(): List<AlbumSummaryResponse> = withContext(Dispatchers.IO){
+        val projections = arrayOf(
+            MediaStore.Images.Media.BUCKET_DISPLAY_NAME
+        )
+
+        val sortOrder = "${MediaStore.Images.Media.BUCKET_DISPLAY_NAME} ASC"
+
+        val cursor: Cursor? = context.contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projections,
+            null,
+            null,
+            sortOrder
+        )
+
+        val albumPhotoCountMap = mutableMapOf<String, Int>()
+
+        cursor?.use {
+            val albumNameColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
+            
+            while (it.moveToNext()) {
+                val albumName = it.getString(albumNameColumn)
+                albumPhotoCountMap[albumName] = (albumPhotoCountMap[albumName] ?: 0) + 1
+            }
+        }
+
+        return@withContext albumPhotoCountMap.map { (albumName, count) ->
+            AlbumSummaryResponse(
+                albumName = albumName,
+                photoCount = count
+            )
+        }
+    }
+
+    override suspend fun getAlbumWithPhotosResponse(albumName: String): AlbumWithPhotosResponse {
+        TODO("Not yet implemented")
+    }
+}
