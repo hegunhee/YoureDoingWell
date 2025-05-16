08fc05fb404ad52e49721063929436af588aefe7
이건희 <leech9876@naver.com>
2025-05-16 09:50:54 +0900

    #76 - feat : PhotoRepository를 상속하는 DefaultPhotoRepository 작성

---
+++ core/data/src/main/java/doingwell/core/data/repository/DefaultPhotoRepository.kt
@@ -0,0 +1,26 @@
+package doingwell.core.data.repository
+
+import com.hegunhee.model.photo.AlbumSummary
+import com.hegunhee.model.photo.AlbumWithPhotos
+import doingwell.core.data.datasource.local.PhotoLocalDataSource
+import doingwell.core.data.mapper.toModel
+import doingwell.core.domain.repository.PhotoRepository
+import javax.inject.Inject
+
+class DefaultPhotoRepository @Inject constructor(
+    private val photoLocalDataSource: PhotoLocalDataSource,
+) : PhotoRepository {
+
+    override suspend fun getAlbumSummaries(): Result<List<AlbumSummary>> {
+        return runCatching {
+            photoLocalDataSource.getAlbumSummaryResponses().toModel()
+        }
+    }
+
+    override suspend fun getAlbumAndPhotos(albumName: String): Result<AlbumWithPhotos> {
+        return runCatching {
+            photoLocalDataSource.getAlbumWithPhotosResponse(albumName).toModel()
+        }
+    }
+
+}
