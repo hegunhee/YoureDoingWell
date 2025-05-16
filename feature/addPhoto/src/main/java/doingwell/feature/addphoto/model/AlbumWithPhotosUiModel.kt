a360bc4bf815e9beec02da5e8168dbdb54342ef5
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-05-16 23:18:12 +0900

    UseCase 및 Ui에서 사용하는 Model 작성 (#79)
    * #78 - feat : 앨범 요약을 가져오는 UseCase 작성
    
    * #78 - feat : 특정 앨범의 사진들을 가져오는 UseCase 작성
    
    * #78 - feat : 사진 관련해서 UiModel 작성

---
+++ feature/addPhoto/src/main/java/doingwell/feature/addphoto/model/AlbumWithPhotosUiModel.kt
@@ -0,0 +1,25 @@
+package doingwell.feature.addphoto.model
+
+import androidx.core.net.toUri
+import com.hegunhee.model.photo.AlbumWithPhotos
+import doingwell.core.ui.text.model.SelectablePhoto
+
+data class AlbumWithPhotosUiModel(
+    val albumName: String,
+    val size: Int,
+    val photos: List<SelectablePhoto>,
+) {
+
+    companion object {
+        fun of(albumWithPhotos: AlbumWithPhotos): AlbumWithPhotosUiModel {
+            with(albumWithPhotos) {
+                return AlbumWithPhotosUiModel(
+                    albumName = albumName,
+                    size = size,
+                    photos = photos.map { SelectablePhoto(it.toUri(), null) }
+                )
+            }
+        }
+    }
+
+}
