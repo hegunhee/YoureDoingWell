837d7b89e594aabe6b04bfaafce9700a4aeedcb1
이건희 <leech9876@naver.com>
2025-05-17 12:52:44 +0900

    #82 - feat : 사진 관련 권한 추가

--- feature/main/src/main/java/doingwell/feature/main/screen/MainRootScreen.kt
+++ feature/main/src/main/java/doingwell/feature/main/screen/MainRootScreen.kt
@@ -1,12 +1,16 @@
 package doingwell.feature.main.screen
 
+import android.Manifest
+import android.os.Build
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.hegunhee.model.user.UserData
 import com.hegunhee.youredoingwell.ui.theme.Typography
 import doingwell.core.ui.text.TitleText
@@ -17,9 +21,17 @@ fun MainScreenRoot(
     popSignInScreen: () -> Unit,
     popDailyScreen: () -> Unit,
 ) {
+    val context = LocalContext.current
+    val photoPermission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        Manifest.permission.READ_MEDIA_IMAGES
+    } else {
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+    
     MainScreen(
         userData = userData,
     )
+    
     LaunchedEffect(userData) {
         if(userData == null) {
             popSignInScreen()
@@ -27,12 +39,20 @@ fun MainScreenRoot(
             popDailyScreen()
         }
     }
+    
+    LaunchedEffect(Unit) {
+        ActivityCompat.requestPermissions(
+            context as android.app.Activity,
+            arrayOf(photoPermission),
+            1000
+        )
+    }
 }
 
 @Composable
 internal fun MainScreen(
-    userData : UserData?,
-    modifier : Modifier = Modifier,
+    userData: UserData?,
+    modifier: Modifier = Modifier,
 ) {
     Column {
         TitleText(
