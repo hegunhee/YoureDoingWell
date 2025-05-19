515809a1d33c464d3b14685206fbad197fa7e6e7
이건희 <leech9876@naver.com>
2025-05-19 16:30:03 +0900

    #86 - feat : 사진 모델 객체와 서버저장용 객체의 매퍼 작성

--- core/data/src/main/java/doingwell/core/data/mapper/photoInfo.kt
+++ core/data/src/main/java/doingwell/core/data/mapper/photoInfo.kt
@@ -3,8 +3,23 @@ package doingwell.core.data.mapper
 import com.hegunhee.model.photo.PhotoInfo
 import doingwell.core.data.datasource.remote.model.photo.PhotoInfoResponse
 
+fun List<PhotoInfoResponse>.toModel() : List<PhotoInfo> {
+    return this.map { it.toModel() }
+}
+
 fun PhotoInfoResponse.toModel() : PhotoInfo {
     return PhotoInfo(
+        url = url,
+        path = path,
+    )
+}
+
+fun List<PhotoInfo>.toEntity() : List<PhotoInfoResponse> {
+    return this.map { it.toEntity() }
+}
+
+fun PhotoInfo.toEntity() : PhotoInfoResponse {
+    return PhotoInfoResponse(
         url = url,
         path = path,
     )
