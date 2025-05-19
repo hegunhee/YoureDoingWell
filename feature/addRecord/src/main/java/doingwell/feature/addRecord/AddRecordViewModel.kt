ba03d185198fe0d562a3ca307055f8bfe33ff5fc
이건희 <leech9876@naver.com>
2025-05-19 17:39:53 +0900

    #88 - feat : 일일기록 저장 로직 작성

--- feature/addRecord/src/main/java/doingwell/feature/addRecord/AddRecordViewModel.kt
+++ feature/addRecord/src/main/java/doingwell/feature/addRecord/AddRecordViewModel.kt
@@ -2,15 +2,27 @@ package doingwell.feature.addRecord
 
 import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hegunhee.model.user.DateTimeInfo
+import com.hegunhee.model.user.record.DailyRecord
 import dagger.hilt.android.lifecycle.HiltViewModel
+import doingwell.core.domain.usecase.photoStorage.UploadPhotoUseCase
+import doingwell.core.domain.usecase.record.InsertDailyRecordUseCase
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
-class AddRecordViewModel @Inject constructor() : ViewModel() {
+class AddRecordViewModel @Inject constructor(
+    private val uplocdPhotoUseCase: UploadPhotoUseCase,
+    private val insertDailyRecordUseCase: InsertDailyRecordUseCase,
+) : ViewModel() {
 
     private val _photos: MutableStateFlow<List<String>> = MutableStateFlow(listOf())
     val photos: StateFlow<List<String>> = _photos.asStateFlow()
@@ -26,5 +38,49 @@ class AddRecordViewModel @Inject constructor() : ViewModel() {
         _photos.update { currentPhotos ->
             currentPhotos - photo
         }
+    }
+
+    fun saveRecord(title: String, description: String, userId: String) {
+        if (title.isEmpty()) return
+
+        val localDateTime = LocalDateTime.now()
+        val dateTimeInfo = createDateTimeInfo(localDateTime)
+
+
+
+        viewModelScope.launch {
+            val uploadedPhotos = try {
+                if (photos.value.isNotEmpty()) {
+                    photos.value.map { photo ->
+                        async { uplocdPhotoUseCase(photo, userId) }
+                    }.awaitAll().map { it.getOrThrow() }
+                } else null
+            } catch (e : Exception) {
+                throw IllegalStateException("사진 저장중 에러가 발생했습니다.")
+            }
+
+            insertDailyRecordUseCase(
+                DailyRecord(
+                    recordId = null,
+                    userId = userId,
+                    title = title,
+                    description = description.ifBlank { null },
+                    photos = uploadedPhotos,
+                    startedAt = dateTimeInfo,
+                    endedAt = dateTimeInfo
+                )
+            )
+
+        }
+    }
+
+    private fun createDateTimeInfo(localDateTime: LocalDateTime): DateTimeInfo {
+        return DateTimeInfo(
+            localDateTime.year,
+            localDateTime.monthValue,
+            localDateTime.dayOfMonth,
+            localDateTime.hour,
+            localDateTime.minute,
+        )
     }
 }
