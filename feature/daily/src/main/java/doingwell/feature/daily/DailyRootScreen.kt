1ee8e8c50cc07accd17858d4b9df3fbf8de2aa4d
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-05-31 11:07:04 +0900

    Daily 화면 뼈대 작성 및 ViewModel 작성 (#97)
    * #96 - feat : Daily 화면에서 보여주던 유저 정보를 삭제하고 오늘의 기록 텍스트를 보여줌
    
    * #96 - feat : DailyViewModel 작성
    
    * #96 - feat : 오늘의 기록을 불러오는 코드 작성
    
    * #96 - feat : 만약 오늘의 기록이 저장되어있지않다면 기록을 조회하지 않도록 미리 처리
    
    * #96 - feat : 오늘의 날씨를 포맷에 맞게 가져오는 DateUtil 작성
    
    * #96 - feat : 사용자 정보와 오늘의 기록정보를 불러오는 뷰모델, UI 코드 작성
    
    추후 코드는 다른 브랜치로 분리해서 작업할 예정

--- feature/daily/src/main/java/doingwell/feature/daily/DailyRootScreen.kt
+++ feature/daily/src/main/java/doingwell/feature/daily/DailyRootScreen.kt
@@ -2,22 +2,41 @@ package doingwell.feature.daily
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.hegunhee.model.user.UserData
+import doingwell.feature.daily.viewModel.DailyViewModel
 
 @Composable
-fun DailyRootScreen(
+internal fun DailyRootScreen(
+    viewModel: DailyViewModel = hiltViewModel(),
     paddingValues: PaddingValues,
-    userData : UserData?,
+    userData: UserData?,
     onClickSignOut: () -> Unit,
 ) {
-    if(userData == null) {
+    if (userData == null) {
         onClickSignOut()
         return
     }
+
+    LaunchedEffect(userData) {
+        viewModel.fetchUserData(userData)
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchDailyRecord()
+    }
+
+    val dailyRecord = viewModel.dailyRecords.collectAsStateWithLifecycle().value
 
     DailyScreen(
         paddingValues,
@@ -27,26 +46,31 @@ fun DailyRootScreen(
 }
 
 @Composable
-fun DailyScreen(
+internal fun DailyScreen(
     paddingValues: PaddingValues,
     userData: UserData,
     onClickSignOut: () -> Unit,
+    modifier: Modifier = Modifier,
 ) {
-    Column {
-        Text(userData.uid)
-        Text(userData.email ?: "")
-        Button({onClickSignOut()}) {
-            Text("로그아웃")
-        }
+    Column(
+        modifier = modifier
+            .padding(paddingValues)
+            .padding(top = 10.dp)
+            .padding(horizontal = 10.dp)
+    ) {
+        Text(
+            stringResource(R.string.daily_record),
+            fontSize = 20.sp
+        )
     }
 }
 
 @Preview
 @Composable
-fun DailyScreenPreview() {
+private fun DailyScreenPreview() {
     DailyScreen(
         paddingValues = PaddingValues(),
-        userData = UserData("TEST_UID","TEST_EMAIL",null),
+        userData = UserData("TEST_UID", "TEST_EMAIL", null),
         onClickSignOut = {},
     )
 }
